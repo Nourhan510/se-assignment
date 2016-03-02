5750b290d3a04411ac2a343cb73f9834d52a65d3
Nourhan510 <nourhan.nader@student.guc.edu.eg>
2016-03-02 10:25:28 +0200

    random color added

--- 31-2277/public/js/main.js
+++ 31-2277/public/js/main.js
@@ -1,10 +1,20 @@
 $(document).ready(function(){
  $("body").click(function(){
  $.get("/api/quote",function(data,status){
- $("body").css('backgroud-color', 'blue')
  $('blockquote.quote').text(data.text);
  $('.author').text(data.author)
  });
+  $("body").css('background-color', getRandomColor());
+
  });
 
 });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
